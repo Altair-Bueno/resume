ffbb77a0e07a7d4bffa310d057e1dd6f280b5477
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-02-19 22:01:03 +0100

    new: Preparser sorts by date

--- scripts/preparser.py
+++ scripts/preparser.py
@@ -9,11 +9,31 @@ __DEFAULT_VALUES = {
     'linkcolor':'magenta',
 }
 
-def set_default_values(data)-> dict:
-    return __DEFAULT_VALUES | data
+def sort_key(element) -> str:
+    if not isinstance(element,dict):
+        return parse('1999-01-01')
+
+    if 'date' in element:
+        return parse(element['date'])
+    elif 'from' in element:
+        return parse(element['from'])
+    else:
+        return parse('1999-01-01')
+
+def sort_by_date(data):
+    if isinstance(data,list):
+        data.sort(reverse=True,key=sort_key)
+    elif isinstance(data,dict):
+        for key in data.keys():
+            sort_by_date(data[key])
+def set_default_values(data:dict):
+    data = __DEFAULT_VALUES | data
+    if 'skills' not in data: data['skills'] = {}
+    if 'cols' not in data['skills']: data['skills']['cols'] = 6
 
 def modify_data(data):
-    data = set_default_values(data)
+    set_default_values(data)
+    sort_by_date(data)
     return  data
 
 def main():
