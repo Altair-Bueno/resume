c90c15d6b2faa36b919bf1b285d2fa1f3bf5e483
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-06-19 20:44:43 +0200

    ref: Improved Makefile

---
+++ Makefile
@@ -0,0 +1,47 @@
+OUT_DIR          = out
+DATA_DIR         = data
+TEMPLATE_DIR     = templates
+
+TARGET           = $(OUT_DIR)/resume.pdf
+TEMPLATE_FILE    = $(TEMPLATE_DIR)/jb2-modern.tex
+PREPARSER_SCRIPT = scripts/preparser.py
+
+MUSTACHE_CC      = poetry run chevron
+MUSTACHE_ARGS    = -l "<<" -r ">>" -w -d
+
+LATEX_DEPS       = enumitem \
+                   sectsty
+
+.PHONY: build deps clean all poetry_deps latex_deps
+.PRECIOUS: $(OUT_DIR)/%.json $(OUT_DIR)/%.tex
+
+# Available targets
+build: $(TARGET)
+deps: poetry_deps latex_deps
+clean: out_clean
+all: deps build
+
+# deps
+poetry_deps:
+	poetry install
+
+latex_deps:
+	sudo tlmgr update --self
+	sudo tlmgr install $(LATEX_DEPS)
+
+# build
+$(OUT_DIR)/%.pdf: $(OUT_DIR)/%.tex $(OUT_DIR)
+	xelatex -output-directory=$(OUT_DIR) $<
+
+$(OUT_DIR)/%.tex: $(OUT_DIR)/%.json $(TEMPLATE_FILE)
+	$(MUSTACHE_CC) $(MUSTACHE_ARGS) $^ > $@
+
+$(OUT_DIR)/%.json: $(OUT_DIR) $(PREPARSER_SCRIPT) $(DATA_DIR)/%.toml
+	poetry run python $(wordlist 2, 3, $^) $@
+
+$(OUT_DIR):
+	mkdir $@
+
+# clean
+out_clean:
+	rm -fr $(OUT_DIR) $(OUT_DIR)
--- scripts/preparser.py
+++ scripts/preparser.py
@@ -1,17 +1,20 @@
 from datetime import datetime
-from dateutil.parser import parse
-from toml import load,dump
+from json import dump
 from sys import argv
+
+from dateutil.parser import parse
+from toml import load
 
 __DEFAULT_VALUES = {
     'fontsize': '10pt',
-    'fontenc':'T1',
-    'urlcolor':'blue',
-    'linkcolor':'magenta',
+    'fontenc': 'T1',
+    'urlcolor': 'blue',
+    'linkcolor': 'magenta',
 }
 
+
 def sort_key(element) -> datetime:
-    if not isinstance(element,dict):
+    if not isinstance(element, dict):
         return datetime.max
 
     result = None
@@ -21,37 +24,45 @@ def sort_key(element) -> datetime:
         result = element['from']
     else:
         result = ''
-    
-    try: return parse(result)
-    except: return datetime.max
+
+    try:
+        return parse(result)
+    except:
+        return datetime.max
+
 
 def sort_by_date(data):
-    if isinstance(data,list):
-        data.sort(reverse=True,key=sort_key)
-    elif isinstance(data,dict):
+    if isinstance(data, list):
+        data.sort(reverse=True, key=sort_key)
+    elif isinstance(data, dict):
         for value in data.values():
             sort_by_date(value)
 
-def set_default_values(data:dict):
+
+def set_default_values(data: dict):
     data = __DEFAULT_VALUES | data
     if 'skills' not in data: data['skills'] = {}
     if 'cols' not in data['skills']: data['skills']['cols'] = 6
+    return data
+
 
 def prepare_data(data):
-    set_default_values(data)
+    data = set_default_values(data)
     sort_by_date(data)
-    return  data
+    return data
+
 
 def main():
-    og_file = open(argv[1],'rt')
-    dst_file = open(argv[2],'wt')
+    og_file = open(argv[1], 'rt')
+    dst_file = open(argv[2], 'wt')
 
     data = load(og_file)
     data = prepare_data(data)
-    dump(data,dst_file)
+    dump(data, dst_file)
 
     og_file.close()
     dst_file.close()
+
 
 if __name__ == '__main__':
     main()
