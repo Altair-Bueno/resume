a446cb206a7a2aa2b6b6fa04f1ca4dd6f4236cf9
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-07-22 20:55:32 +0200

    new: Using `typer` to improve CLI experience

--- scripts/preparser.py
+++ scripts/preparser.py
@@ -1,25 +1,22 @@
 from json import dump
-from sys import argv
 
+from typer import run
 from yaml import load, SafeLoader
 
 from src.model.jsonresume import ResumeSchema
 from src.model.template import TemplateScheme
 
 
-def main():
-    origin_filename = argv[1]
-    destination_filename = argv[2]
-
-    with open(origin_filename, "r") as origin:
+def main(data: str, output: str):
+    with open(data, "r") as origin:
         d = load(origin, Loader=SafeLoader)
 
     jsonresume = ResumeSchema(**d)
     data = TemplateScheme.from_jsonresume(jsonresume)
 
-    with open(destination_filename, "w") as destination:
+    with open(output, "w") as destination:
         dump(data.dict(), destination, indent=2)
 
 
 if __name__ == "__main__":
-    main()
+    run(main)
